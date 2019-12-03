048ae7ebc4f3fe6381333670a527e5f9b8368fed
Dmitriy <dmv.kirillov@gmail.com>
2019-12-03 12:30:10 +0300

    add mobile version of gui to game view

---
+++ battleship/wwwroot/js/GameProcess/scriptGenBattlefield.js
@@ -0,0 +1,29 @@
+function redrawBattlefield() {
+	battlefieldLocation(); 
+	genPlaceShipStat('player-ships-stat');
+	genPlaceShipStat('opponent-ships-stat');
+}
+
+function battlefieldLocation() {
+	const battlefieldId = "battlefield";
+	var elParent = document.getElementById(battlefieldId);
+
+	if (elParent.offsetWidth < elParent.offsetHeight) {
+		elParent.classList.toggle("flex-column", true);
+		document.querySelectorAll("#battlefield > div").forEach(function(el) {
+			el.style.width = "100%";
+			el.style.height = "50%";
+
+		});
+
+		var playerBattlefield = document.getElementById("player-battlefield");
+		var opponentBattlefield = document.getElementById("opponent-battlefield");
+		playerBattlefield.style.justifyContent = "center";
+		playerBattlefield.style.display = "flex";
+		opponentBattlefield.style.justifyContent = "center";
+		opponentBattlefield.style.display = "flex";
+	} else  {
+		elParent.classList.toggle("flex-column", false);
+	}
+
+}
